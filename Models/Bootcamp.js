81cd44b6a5f610ffaa57ceb6d64c5d5a5ab0b22b
Bryan Jazo <bryanjazo88@gmail.com>
2021-09-07 21:27:38 -0700

    finished hooks fot models slugs

--- Models/Bootcamp.js
+++ Models/Bootcamp.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify')
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -109,5 +109,10 @@ const BootcampSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
-
+// middleware create bootcamp slug fromt he name 
+BootcampSchema.pre('save', function(next){
+  console.log('Slugify Ran', this.name)
+  this.slug = slugify(this.name, {lower: true, })
+  next()
+})
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,56 +1,48 @@
 const Bootcamp = require('../models/Bootcamp')
 const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/async')
 // @desc    Get request for all bootcamps 
 // @route   GET /api/va1/bootcamps
 // @access  Public 
-exports.getBootcamps = async (req, resp, next) =>{
-    try {
+exports.getBootcamps = asyncHandler(async(req, resp, next) =>{
+    
         const bootcamps = await Bootcamp.find()
 
         resp.status(200).json({success: true, count: bootcamps.length, data: bootcamps})
-    } catch (error) {
-       
-        next(error)
-    }
-}
+
+})
 
 // @desc    Get request for single bootcamp
 // @route   GET /api/va1/bootcamps/:id
 // @access  Public 
-exports.getBootcamp = async (req, resp, next) =>{
-   try {
+exports.getBootcamp = asyncHandler(async(req, resp, next) =>{
+
       const bootcamp = await Bootcamp.findById(req.params.id) 
       if(!bootcamp){
         return next(new ErrorResponse(`Bootcamp Not Found With Id Of ${req.params.id}`, 404))
       }
       resp.status(200).json({success: true, data: bootcamp})
-   } catch (error) {
-    console.log(error)
-      next(error)
-   }
-}
+  
+})
 
 // @desc    Post request for all bootcamps 
 // @route   POST /api/va1/bootcamps
 // @access  Private 
-exports.createBootcamps = async (req, resp, next) =>{
-    try {
+exports.createBootcamps = asyncHandler (async(req, resp, next) =>{
+  
         const bootcamp = await Bootcamp.create(req.body)
     
         resp.status(201).json({
             success: true,
             data: bootcamp
         })
-    } catch (error) {
-        next(error)
-    }
-}
+  
+})
 
 // @desc    Update request for one bootcamp 
 // @route   UPDATE /api/va1/bootcamps/:id
 // @access  Private 
-exports.updateBootcamp = async (req, resp, next) =>{
-    try {
+exports.updateBootcamp = asyncHandler (async (req, resp, next) =>{
         const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id,req.body, {
             new: true,
             runValidators: true
@@ -59,23 +51,18 @@ exports.updateBootcamp = async (req, resp, next) =>{
             return next(new ErrorResponse(`Bootcamp Not Found With Id Of ${req.params.id}`, 404))
         }
         resp.status(200).json({success: true, data: bootcamp})
-    } catch (error) {
-        next(error)
-    }
-}
+  
+})
 // @desc    delete request for one bootcamp 
 // @route   DELETE /api/va1/bootcamps/:id
 // @access  Private 
-exports.deleteBootcamp = async (req, resp, next) =>{
-    try {
+exports.deleteBootcamp = asyncHandler (async (req, resp, next) =>{
+
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
         if(!bootcamp){
             return next(new ErrorResponse(`Bootcamp Not Found With Id Of ${req.params.id}`, 404))
         }
         resp.status(200).json({success: true, data: {}})
-    } catch (error) {
-        next(error)
-    }
-}
+})
 
